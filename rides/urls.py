a5da9b8e9dfe5412fc9e63c00f60f1d7e2f74e16
Ramiro Uvalle <ramiro.uvallevgr@uanl.edu.mx>
2017-09-25 23:07:29 -0500

    Ride detail view added

--- rides/urls.py
+++ rides/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'rides'
 urlpatterns = [
     url(r'^$', views.RidesListView.as_view(), name="rides"),
+    url(r'^(?P<pk>[0-9]+)', views.RideDetailView.as_view(), name="ride_detail")
 ]
--- rides/views.py
+++ rides/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Ride
 
@@ -15,3 +15,12 @@ class RidesListView(ListView):
 
     def get_queryset(self):
         return Ride.objects.all()
+
+class RideDetailView(DetailView):
+    model = Ride
+    template_name = 'rides/ride_detail.html'
+    context_object_name = 'ride'
+
+    def get_context_data(self, **kwargs):
+        context = super(RideDetailView, self).get_context_data(**kwargs)
+        return context
