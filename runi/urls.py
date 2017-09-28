68df442dfb6307654ac6b4aa695c50f3895b0cdb
ramirouvalle <ramiro.uvallevgr@uanl.edu.mx>
2017-09-28 14:25:58 -0500

    Renaming main project

--- runi/urls.py
+++ runi/urls.py
@@ -1,4 +1,4 @@
-"""rides_app URL Configuration
+"""runi URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
