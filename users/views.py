08baee3033e4e15e4afbcfe79dfa490e3318e155
ramirouvalle <ramiro.uvallevgr@uanl.edu.mx>
2017-12-08 00:17:12 -0600

    Login form created

--- users/views.py
+++ users/views.py
@@ -1,5 +1,25 @@
+from django.contrib.auth import authenticate
 from django.shortcuts import render
+
+from .forms import LoginForm
 
 
 def login(request):
-    return render(request, 'users/login.html')
+
+    if request.method == 'POST':
+        print(request.POST)
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            print('valid')
+            user = authenticate(username=request.POST['username'], password=request.POST['password'])
+            if user is not None:
+                print('user exists')
+            else:
+                print('user does not exists')
+        else:
+            print('invalid')
+    else:
+        form = LoginForm()
+
+    return render(request, 'users/login.html', {'form': form})
